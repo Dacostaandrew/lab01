77f869f2dbcd5c24825aee011a7cde3ab7e0a349
dacostaandrew <dacostaandrew@gwmail.gwu.edu>
2016-01-14 13:24:19 +0000

    hello.c

--- hello.c
+++ hello.c
@@ -7,5 +7,21 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello world!\n");
+	printf("By Andrew da Costa\n");
+	
+	int x,y,z;
+	x = 10;
+	y = 20;
+	z = x + y;
+	printf("x is %d, y is %d, and z is %d \n", x, y, z);
+	
+	int h,w,d,V;
+	h = 11;
+	w = 7;
+	d = 14;
+	V = h * w * d; /*This calculates the volume of a box with given dimensions h,w,d*/
+	printf("The box with dimensions %d by %d by %d, has a volume of %d\n",h,w,d,V);
+	
+	
 	return 0;
 }
